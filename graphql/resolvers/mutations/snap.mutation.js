2a94e305d7f07c1b132c433fc2ef0c9b9c9157a6
Suleyman Sevimli <suleyman.sevimli1@gmail.com>
2020-07-30 12:20:35 +0300

    snap delete added

--- graphql/resolvers/mutations/snap.mutation.js
+++ graphql/resolvers/mutations/snap.mutation.js
@@ -1,9 +1,11 @@
 module.exports = {
     createSnap : async (parent, { data : { text,user_id,category_id } } , { Snap }) => {
-
         return await new Snap({
             text,user_id,category_id
         }).save()
+    },
 
+    deleteSnap : async ( parent, args, { Snap } ) => {
+        return Snap.findByIdAndDelete(args.id);
     }
 }
